dbeb0c669e56158b00f865dc4b2714240e78a4ed
unknown <cesartole73@gmail.com>
2019-11-23 00:01:21 -0600

    added average change and max/min capabilities, finished PyBank

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,8 +4,6 @@ import os
 
 csv_path = os.path.join('budget_data.csv')
 
-print(csv_path)
-
 with open(csv_path, 'r') as infile: # not sure on language for this one
     in_c = csv.reader(infile)   #or this one :(
 
@@ -17,9 +15,38 @@ with open(csv_path, 'r') as infile: # not sure on language for this one
     months = 0
     for i in data:
         months += 1
-       
-    
-    
 
-    print('Timeframe of analysis:  ' + str(months) + ' months')
-    p.pprint(data)
+#net profit
+    profit = 0
+    for i in range(0, len(data)):
+        num = data[i][1]
+        profit += int(num)
+
+#Mst answer average change greatest increase and greatest decrease then done :)
+#left off in min 21:46
+
+#attempt for average change
+    total_diff = 0
+    for i in range(1, len(data)):
+        num1 = int(data[i-1][1])
+        num2 = int(data[i][1])
+        diff = num2 - num1
+        total_diff += int(diff)
+
+#max/min
+    maxminlist = []
+    for i in range(0, len(data)):
+        num = int(data[i][1])
+        maxminlist.append(num)
+
+    maxnum = max(maxminlist)
+    minnum = min(maxminlist)
+
+    maxindex = maxminlist.index(maxnum)
+    minindex = maxminlist.index(minnum)
+
+print('Timeframe of analysis:  ' + str(months) + ' months')
+print('Total profit:  $' + str(profit))
+print('Average Change: $' + str(format(float(total_diff / 85), '.2f')))
+print('Greatest Increase in Profits: ' + str(data[maxindex][0]) + ' $' + str(maxnum))
+print('Greatest Decrease in Profits: ' + str(data[minindex][0]) + ' $' + str(minnum))
